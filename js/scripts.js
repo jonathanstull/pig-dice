89aa1541d32a025fbec4507f8a1f798393210cfa
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-04-07 11:34:25 -0700

    pass second test creating a player object constructor
    Co-authored-by: Noah Woodrum <heymymomthinksimcool@gmail.com>
    Co-authored-by: Brandon Magofna <Magofna68@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// Business logic
+
+function PlayerConstructor(playerId, turnTotal, playerScore) {
+  this.playerId = playerId;
+  this.turnTotal = turnTotal;
+  this.playerScore = playerScore;
+}
+
+// User interface logic
+
